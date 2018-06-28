e2b3d5475b7428f52c1c404687fd0a88e35d41ca
Carla Weiss <carlacfw@gmail.com>
2018-06-28 14:40:38 +1200

    Add click handling to the Squares

--- src/components/Square/index.js
+++ src/components/Square/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { isUndefined } from 'ramda-adjunct'
 
 const StyledSquare = styled.div`
   border-color: #00e6e6;
@@ -7,6 +8,7 @@ const StyledSquare = styled.div`
   border-width: 0 ${({ index }) => (index % 3 === 2 ? 0 : '2px')}
     ${({ index }) => (index < 6 ? '2px' : 0)} 0;
   color: ${({ player }) => (player === 'x' ? '#ff0066' : '#ff3300')};
+  cursor: ${({ onClick }) => (isUndefined(onClick) ? 'default' : 'pointer')};
   font-size: 16vh;
   font-weight: bold;
   line-height: 20vh;
@@ -15,10 +17,10 @@ const StyledSquare = styled.div`
 `
 StyledSquare.displayName = 'StyledSquare'
 
-export default function Square (props) {
+export default function Square ({ handleClick, index, player }) {
   return (
-    <StyledSquare index={props.index} player={props.player}>
-      {props.player}
+    <StyledSquare index={index} player={player} onClick={handleClick}>
+      {player}
     </StyledSquare>
   )
 }

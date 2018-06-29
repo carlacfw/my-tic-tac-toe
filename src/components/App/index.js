cb48f5b5f655bb43af750ea1b90be9da5c48c3bd
Carla Weiss <carlacfw@gmail.com>
2018-06-29 17:41:11 +1200

    Add click handler to squares, update snapshots

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import styled from 'styled-components'
 import { times } from 'ramda'
+import { isUndefined } from 'ramda-adjunct'
 
 import { Board, Square } from '..'
+import { getPlayer } from '../../utilities'
 
 /* The times function takes a function and an integer n,
  and returns an array of length n in which each item is created by calling the function
  which is passed the index of that cell in the array.
  So the first cell, the function is called with 0, the second cell it is called with 1, etc. */
 // and using an even/odd function to alternate between player X and player O as we create the squares
-const makeSquares = () =>
-  times(
-    idx => <Square key={idx} index={idx} player={idx % 2 === 0 ? 'x' : 'o'} />,
-    9
-  )
+
+function makeSquares (moves) {
+  return times(square => {
+    const player = getPlayer(square, moves)
+
+    return isUndefined(player) ? (
+      <Square
+        key={square}
+        index={square}
+        handleClick={() => console.log(`Square ${square}`)}
+      />
+    ) : (
+      <Square key={square} index={square} player={player} />
+    )
+  }, 9)
+}
 
 const StyledApp = styled.div`
   display: grid;
@@ -26,11 +39,10 @@ const StyledApp = styled.div`
 `
 StyledApp.displayName = 'StyledApp'
 
-export default function App () {
+export default function App ({ moves = [] }) {
   return (
-    // drawing the board
     <StyledApp>
-      <Board>{makeSquares()}</Board>
+      <Board>{makeSquares(moves)}</Board>
     </StyledApp>
   )
 }

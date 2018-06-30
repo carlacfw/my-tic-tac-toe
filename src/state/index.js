a39b36c9d345cac7d8031720e891704febefd0b0
Carla Weiss <carlacfw@gmail.com>
2018-06-30 15:28:42 +1200

    Add the store

--- src/state/index.js
+++ src/state/index.js
@@ -2,5 +2,13 @@ import { SQUARE_CLICKED } from './constants'
 import { squareClicked } from './actions'
 import { initialState, rootReducer } from './reducers'
 import { getMoves } from './selectors'
+import configureStore from './store'
 
-export { getMoves, initialState, rootReducer, SQUARE_CLICKED, squareClicked }
+export {
+  configureStore,
+  getMoves,
+  initialState,
+  rootReducer,
+  SQUARE_CLICKED,
+  squareClicked
+}

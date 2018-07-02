97b790cc666d2308f27d387838ce6d84c97a4d74
Carla Weiss <carlacfw@gmail.com>
2018-07-02 15:04:39 +1200

    Update reducer for GAME_OVER action

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,14 +1,20 @@
 import { isUndefined } from 'ramda-adjunct'
 
-import { SQUARE_CLICKED } from '..'
+import { GAME_OVER, SQUARE_CLICKED } from '..'
 
 const initialState = { moves: [] }
 
 function rootReducer (state = initialState, { payload = {}, type }) {
+  const { square, winners: { squares, player } = {} } = payload
   switch (type) {
+    case GAME_OVER:
+      return {
+        ...state,
+        winningSquares: squares,
+        winningPlayer: player
+      }
     case SQUARE_CLICKED:
       return {
-        // remember, the ... is copying the old state that will be changed
         ...state,
         moves: isUndefined(payload.square)
           ? state.moves
@@ -20,3 +26,4 @@ function rootReducer (state = initialState, { payload = {}, type }) {
 }
 
 export { initialState, rootReducer }
+// remember, the ...state is copying the old state that will be changed
--- src/state/reducers/index.spec.js
+++ src/state/reducers/index.spec.js
@@ -1,11 +1,12 @@
 import { initialState, rootReducer } from '.'
-import { squareClicked } from '..'
+import { gameOver, squareClicked } from '..'
 
-describe('state:reducer', () => {
+describe('state:reducers', () => {
   describe('rootReducer', () => {
     it('defaults to the initialState', () => {
       expect(rootReducer(undefined, {})).toBe(initialState)
     })
+
     it('handles an unknown action type by returning the state unchanged', () => {
       const state = 'state'
 
@@ -21,6 +22,7 @@ describe('state:reducer', () => {
         moves: [4, 0, 2]
       })
     })
+
     it('returns the state unchanged when the square is not supplied', () => {
       const state = {
         moves: [4, 0]
@@ -28,6 +30,18 @@ describe('state:reducer', () => {
 
       expect(rootReducer(state, squareClicked())).toMatchObject({
         moves: [4, 0]
+      })
+    })
+
+    it('adds the winningSquares and the winningPlayer, if any, on GAME_OVER', () => {
+      const state = {
+        moves: [0, 1, 2, 3, 4, 5, 6]
+      }
+
+      expect(rootReducer(state, gameOver([2, 4, 6], 'x'))).toMatchObject({
+        moves: [0, 1, 2, 3, 4, 5, 6],
+        winningSquares: [2, 4, 6],
+        winningPlayer: 'x'
       })
     })
   })
